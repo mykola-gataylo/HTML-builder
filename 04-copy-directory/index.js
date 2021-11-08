03491f0c1859e1983c5420cbf05c264663402cee
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-11-08 18:11:19 +0100

    feat: feat solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcFolder = path.join(__dirname, 'files');
+const outFolder = path.join(__dirname, 'files-copy');
+
+const copyFolder = () => {
+  fs.mkdir(outFolder, () => {
+    fs.readdir(srcFolder, (err, files) => {
+      files.map((file) => {
+        fs.copyFile(
+          path.join(srcFolder, file),
+          path.join(outFolder, file),
+
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      });
+    });
+  });
+};
+
+copyFolder();
