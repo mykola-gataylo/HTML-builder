09f946b0dd6b7e022cb03d8abbeb1a370f91c429
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-11-09 11:40:27 +0100

    fix: fix error with deleting file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const fsPromises = fs.promises;
 
 const srcFolder = path.join(__dirname, 'files');
 const outFolder = path.join(__dirname, 'files-copy');
 
-const copyFolder = () => {
-  fs.mkdir(outFolder, () => {
+const copyFolder = async () => {
+  try {
+    await fsPromises.rm(outFolder, { recursive: true });
+  } catch (error) {
+    console.log('Error: Folder not found', error.code);
+  }
+  fs.mkdir(outFolder, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
     fs.readdir(srcFolder, (err, files) => {
       files.map((file) => {
         fs.copyFile(
